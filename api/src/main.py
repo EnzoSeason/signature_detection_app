2d8fd68dbe237e39f4b43d5a2fbed021a5af13e3
Jijie LIU <liujijieseason@gmail.com>
2021-05-29 19:51:02 +0200

    feat: remove all the endpoints then add api

--- api/src/main.py
+++ api/src/main.py
@@ -1,15 +1,7 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/api/")
+def read_api():
+    return {"api": "hi :)"}
