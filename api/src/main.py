ae2cf8ffb30ebda84c2c98bd89c94ce490fe089e
Jijie LIU <liujijieseason@gmail.com>
2021-05-29 19:53:26 +0200

    feat: add a new endpoint, signature-detect

--- api/src/main.py
+++ api/src/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/api/")
 def read_api():
     return {"api": "hi :)"}
+
+
+@app.get("/api/signature-detect/")
+def read_signature_detect():
+    return {"signature": "Found"}
