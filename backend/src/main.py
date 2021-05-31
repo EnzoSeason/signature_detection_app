fe89a4b3c957d581d82317c07c0799f11c6bd257
Jijie LIU <liujijieseason@gmail.com>
2021-05-31 11:15:22 +0200

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 async def create_file(file: UploadFile = File(...)):
     if not file:
         raise HTTPException(status_code=404, detail="File not found")
-    print(file.content_type)
+    
     if file.content_type != "image/jpeg" and file.content_type != "image/png":
         raise HTTPException(
             status_code=404, detail="File type must be image/png or image/jpeg"
