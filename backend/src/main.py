b8a41037566325ffe464b4b2ca0d818d83398022
Jijie LIU <liujijieseason@gmail.com>
2021-05-31 10:39:46 +0200

    feat: read upload file

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
 
 app = FastAPI()
 
 
-@app.get("/api/")
-def read_api():
-    return {"api": "hi :)"}
+@app.post("/api/")
+async def create_file(file: UploadFile = File(...)):
+    if not file:
+        raise HTTPException(status_code=404, detail="File not found")
+    print(file.content_type)
+    if file.content_type != "image/jpeg" and file.content_type != "image/png":
+        raise HTTPException(
+            status_code=404, detail="File type must be image/png or image/jpeg"
+        )
 
-
-@app.get("/api/signature-detect/")
-def read_signature_detect():
-    return {"signature": "Found"}
+    return {"filename": file.filename}
